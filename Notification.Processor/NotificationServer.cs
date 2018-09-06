966b34e018e9520fcde0f2ee16721dee928f3eed
kevinduongla <kevinduongla@gmail.com>
2018-09-07 11:30:35 +1200

    first commit

---
+++ Notification.Processor/NotificationServer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Transactions;
+
+namespace Notification.Processor
+{
+    class NotificationServer: INotificationServer
+    {
+        private bool _shouldWork;
+        private int _clock=1;
+        public void Initialize()
+        {
+            _shouldWork = true;
+        }
+        public void Start()
+        {
+            var workingThread = new Thread(new ThreadStart(SendNotification));
+            workingThread.Start();
+        }
+        void SendNotification()
+        {
+            Console.Write("Start Sending Notification \n");
+
+            do
+            {
+                try
+                {
+                    // declare the transaction options
+                    var transactionOptions = new TransactionOptions
+                    {
+                        IsolationLevel = IsolationLevel.ReadUncommitted
+                    };
+                   
+                    using (var transScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions))
+                    {
+
+                        Console.Write("Testing " + _clock.ToString() + "\n");
+
+
+
+
+
+
+                        transScope.Complete();
+                        _clock++;
+
+                        //if (_clock == 10)
+                        //{
+                        //    _shouldWork = false;
+                        //}
+                    }
+                }
+                catch (Exception x)
+                {
+                    Console.Write("transScope.Complete exception, sleeping 10 sec", x);
+                    Thread.Sleep(10000);
+
+                }
+
+                Thread.Sleep(Settings.Default.FetchingIntervalSeconds);
+
+            } while (_shouldWork);
+        }
+        public void Stop()
+        {
+            _shouldWork = false;
+        }
+        public void Pause()
+        {
+        }
+        public void Resume()
+        {
+        }
+
+    }
+}
---
+++ Notification.Processor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Topshelf;
+
+namespace Notification.Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host host = HostFactory.New(c =>
+            {
+                c.Service<NotificationServer>(s =>
+                {
+                    s.ConstructUsing(builder =>
+                    {
+                        var server = new NotificationServer();
+                        server.Initialize();
+                        return server;
+                    });
+
+                    s.WhenStarted(server => server.Start());
+                    s.WhenPaused(server => server.Pause());
+                    s.WhenContinued(server => server.Resume());
+                    s.WhenStopped(server => server.Stop());
+                });
+
+                c.RunAsLocalSystem();
+                c.SetDescription("Log Processor");
+                c.SetDisplayName("Log Processor");
+                c.SetServiceName("NotificationProcessor");
+            });
+
+            host.Run();
+        }
+    }
+}
